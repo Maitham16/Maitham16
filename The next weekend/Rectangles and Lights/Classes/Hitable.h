4d017f4ff14d382c56e3682c1edf55d6b7711fb4
Maitham Alrubaye <maythem.altamar@gmail.com>
2023-07-11 22:27:02 +0200

    update to fix some issues

--- The next weekend/Rectangles and Lights/Classes/Hitable.h
+++ The next weekend/Rectangles and Lights/Classes/Hitable.h
@@ -4,6 +4,7 @@
 
 #include "Ray.h"
 #include "AABB.h"
+#include <vector>
 
 class Material; // forward declaration
 
@@ -20,8 +21,37 @@ struct Hit_record
 class Hitable
 {
 public:
+    std::vector<Hitable *> objects;
+    Vec3 center;
+
     virtual bool intersect(const Ray &ray, float t_min, float t_max, Hit_record &hit_record) const = 0;
     virtual bool bounding_box(float tm0, float tm1, AABB &box) const = 0;
+
+    // add a Hitable object to the list
+    void addObject(Hitable *obj)
+    {
+        objects.push_back(obj);
+    }
+
+    // iterate over the list of Hitable objects
+    const std::vector<Hitable *> &getObjects() const
+    {
+        return objects;
+    }
+
+    // is light source
+    bool is_light_source() const
+    {
+        // check whether the object is a light source
+        if (objects.size() > 0)
+        {
+            return objects[0]->is_light_source();
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
 
 #endif
